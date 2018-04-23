c74a0cafbfe6565ba3438f44f0027c7ee81800c4
Mairesu <marusnak@online.no>
2018-04-23 12:14:43 +0200

    JS doc
    and made shop unlocks based on lifetime cats

--- CatGameScripts.js
+++ CatGameScripts.js
@@ -1,17 +1,24 @@
+
+//Game logic constants
 var UPDATETIME = 100;
 
+//Global cat variables
 var cats = 0;
 var lifetimeCats = 0;
 
+//Catnip variables
 var catnip = 0;
 var catnipPrice = 100;
 var catnipUnlocked = false;
 
+//Cat lady variables
 var catLadies = 0;
 var catLadiesPrice = 250;
 var catLadiesUnlocked = false;
 
-
+/**
+ * Increases the amount of cats when the player clicks on the cat
+ */
 function increaseCatsOnClick() {
     var catIncrease = Math.exp(0.0291 * catnip);
     cats += catIncrease;
@@ -19,15 +26,24 @@ function increaseCatsOnClick() {
     updateCats();
 }
 
+/**
+ * Passively increases cats depending
+ */
 function increaseCats() {
     var catIncrease = (Math.exp(0.0433 * catLadies) - 1);
     cats += catIncrease;
     lifetimeCats += catIncrease;
     updateCats();
 }
+
+/**
+ * Runs the increaseCats function every "UPDATETIME" milliseconds
+ */
 setInterval(increaseCats, UPDATETIME);
 
-
+/**
+ * Updates the cats to the html display and unlocks shop functions when certain cat milestones are reached
+ */
 function updateCats()   {
     document.getElementById("catCount").innerHTML = Math.floor(cats);
     document.getElementById("lifetimeCatCount").innerHTML = Math.floor(lifetimeCats);
@@ -39,8 +55,11 @@ function updateCats()   {
     }
 }
 
+/**
+ * unlocks catnip and writes it to the html display
+ */
 function unlockCatnip() {
-    if(100 <= cats) {
+    if(100 <= lifetimeCats) {
         document.getElementById("catnipShop").innerHTML =
             '<li class="shopElement shopButtonContainer">\n' +
             '        <button class="shopButton" onclick="buyCatnip()">ᐃ</button>\n' +
@@ -58,6 +77,10 @@ function unlockCatnip() {
     }
 }
 
+/**
+ * makes buying catnip possible
+ * increases price of catnip the more total catnip the player has
+ */
 function buyCatnip()    {
     if(cats >= catnipPrice)    {
         cats -= catnipPrice;
@@ -69,8 +92,11 @@ function buyCatnip()    {
     }
 }
 
+/**
+ * unlocks cat ladies and writes it to the html display
+ */
 function unlockCrazyCatLadies() {
-    if (250 <= cats) {
+    if (250 <= lifetimeCats) {
         document.getElementById("catLadyShop").innerHTML =
             ' <li class="shopElement shopButtonContainer">\n' +
             '        <button class="shopButton" onclick="buyCatLadies()">ᐃ</button>\n' +
@@ -88,6 +114,10 @@ function unlockCrazyCatLadies() {
     }
 }
 
+/**
+ * makes the purchase of crazy cat ladies possible
+ * increases price of cat ladies the more total catladies the player has
+ */
 function buyCatLadies()    {
     if(cats >= catLadiesPrice)    {
         cats -= catLadiesPrice;
